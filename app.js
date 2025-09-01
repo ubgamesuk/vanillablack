283a58596e993c69648d122d7fd052fc5a457218
ubgamesuk <tlowdon10@outlook.com>
2025-09-01 13:13:05 +0100

    Add all files

---
+++ app.js
@@ -0,0 +1,16 @@
+     function closeNavigation() {
+      var x = document.getElementById("navmenu");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
+        function openNavigation() {
+      var x = document.getElementById("navmenu");
+      if (x.style.display === "none") {
+        x.style.display = "block";
+      } else {
+        x.style.display = "none";
+      }
+    }
